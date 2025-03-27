43cb9cd62edc8a213ce289815a6f9b16cf5599e0
twahir21 <twahirsudy3@gmail.com>
2025-03-27 17:40:59 +0300

    protecting suppliers get all route

--- plugin/supplier.ts
+++ plugin/supplier.ts
@@ -1,6 +1,8 @@
 import Elysia from "elysia";
 import { suppDel, suppGet, suppGetOne, suppPost, suppPut } from "../functions/suppFunc";
 import jwt from "@elysiajs/jwt";
+import { getTranslation } from "../functions/translation";
+import { extractId } from "../functions/security/jwtToken";
 
 const JWT_SECRET = process.env.JWT_TOKEN || "something@#morecomplicated<>es>??><Ess5%";
 
@@ -10,7 +12,21 @@ const suppPlugin = new Elysia()
         name: 'jwt',
         secret: JWT_SECRET
     }))
-    .get("/suppliers", suppGet)
+    .get("/suppliers", async ({ jwt, cookie, headers}) => {
+            const { userId, shopId } = await extractId({ jwt, cookie });
+            const lang: any = headers["accept-language"]?.split(",") || "sw";
+            const token = cookie.auth?.value;
+            if (!token) {
+                throw new Error(`${await getTranslation(lang, "noToken")}`)
+            }
+    
+            const decoded = await jwt.verify(token)
+            if (!decoded) {
+                throw new Error("Unauthorized -  invalid token ")
+            }
+
+            return suppGet
+        })
     .post("/suppliers/:shopId", suppPost)
     .put("/suppliers/:id", suppPut)
     .delete("/suppliers/:id", suppDel)
